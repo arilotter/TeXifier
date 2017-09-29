51987f6fca75970eaf41a04f6914df622a55ed12
Ari Lotter <100621763@oncampus.local>
2017-09-29 11:28:56 -0400

    body parser urlencoded

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/', function(request, response) {
+app.post('/', function (request, response) {
   console.log(request.body);
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
