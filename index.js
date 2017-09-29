a2cc5c56fc68d8a371806ab7f8b16caba609eb90
Ari Lotter <100621763@oncampus.local>
2017-09-29 11:55:48 -0400

    mathjax

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mj = require("mathjax-node");
+mj.config({
+  MathJax: {
+    // traditional MathJax configuration
+  }
+});
+mj.start();
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/', function (request, response) {
-  console.log(request.body);
+  console.log(request.body.text);
+  mj.typeset({
+    math: request.body.text,
+    format: "inline-TeX",
+    svg: true
+  }, (data) => {
+    if(data.errors) {
+      console.log("error! couldn't render " + request.body.text);
+      response.status(500).send("something broke!");
+    } else {
+      response.status(200).send(data.svg)
+    }
+  })
 });
 
 app.listen(app.get('port'), function () {
