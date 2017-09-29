677152aa4bec867612e1d313e623a02cfc170057
Ari Lotter <100621763@oncampus.local>
2017-09-29 12:03:28 -0400

    use remote renderer

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mj = require("mathjax-node");
-mj.config({
-  MathJax: {
-    // traditional MathJax configuration
-  }
-});
-mj.start();
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", process.env.PORT || 5000);
 
-app.post('/', function (request, response) {
+app.post("/", function(request, response) {
   console.log(request.body.text);
-  mj.typeset({
-    math: request.body.text,
-    format: "inline-TeX",
-    svg: true
-  }, (data) => {
-    if(data.errors) {
-      console.log("error! couldn't render " + request.body.text);
-      response.status(500).send("something broke!");
-    } else {
-      response.status(200).send(data.svg)
+  request(
+    {
+      url: "http://quicklatex.com/latex3.f/",
+      method: "post",
+      form: {
+        formula: request.body.text,
+        fsize: "20px",
+        fcolor: "000000",
+        mode: "0",
+        out: "1",
+        remhost: "quicklatex.com",
+        preamble:
+          "\\usepackage{amsmath} \\usepackage{amsfonts} \\usepackage{amssymb} \\usepackage{graphicx} \\usepackage{mhchem}",
+        errors: "1"
+      }
+    },
+    (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return response.status(500).send("something broke!");
+      }
+      var image = body.split("\n")[1].split(" ")[0];
+
+      var params = {
+        unfurl_media: true,
+        response_type: "in_channel",
+        attachments: [
+          {
+            color: "#36a64f",
+            fallback: "quicklatex.com",
+            footer: "quicklatex.com",
+            image_url: image
+          }
+        ]
+      };
+      response.status(200).send(params);
     }
-  })
+  );
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
 });
