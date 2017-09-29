4170407c84f7ba1a59873a4c775ee521ea4b8abb
Ari Lotter <100621763@oncampus.local>
2017-09-29 11:27:09 -0400

    oisdg

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/', function(request, response) {
-  console.log(request);
+  console.log(request.body);
 });
 
 app.listen(app.get('port'), function() {
